4875c4b38b3734f38a1ee7de4b18c4e4b932fc0b
Cameron James <cjames0815@gmail.com>
2023-10-20 10:53:17 -0400

    First Version Collection

--- CMSC-251-DataScructures/collection-classes/src/node/node.py
+++ CMSC-251-DataScructures/collection-classes/src/node/node.py
@@ -14,3 +14,36 @@ class node:
         """        
         self.__data = data
         self.__link = link
+
+    def getData(self):
+        """Returss the data value stored in the calling node
+
+        Returns:
+            : data value stored  in the calling node
+        """        
+        return self.__data
+    
+    def setData(self, data):
+        """Sets the data value stored in the calling node to the 
+        specified data value
+
+        Args:
+            data (_type_): specified data value
+        """        
+        self.data = data
+
+    def getLink(self):
+        """Returns the link stored in the calling node
+
+        Returns:
+            node: link stored in the calling node
+        """        
+        return self.__link
+    
+    def setLink(self,link):
+        """Sets the link stored in the calling node to the specified link.
+
+        Args:
+            link (node): specified link
+        """        
+        self.__link = link
